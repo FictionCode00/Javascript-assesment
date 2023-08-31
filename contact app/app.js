85dd0ed9e3e33ba9d2720169b651c6287b60921e
FictionCode00 <sumitkumar19016@gmail.com>
2023-08-31 15:26:34 +0530

    change api route

--- contact app/app.js
+++ contact app/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/contacts', contactsRouter);
+app.use('/contacts', contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
